584a05c3ddcaea8af95d846c8654df13cf2ce8ac
Jacko0b <jakub.sobota.pl@gmail.com>
2022-03-26 13:55:04 +0100

    Building project files

---
+++ src/components/Attribution.js
@@ -0,0 +1,10 @@
+const Attribution = () => {
+  return (
+    <div className="attribution" >
+       Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+       Coded by <a href="https://www.linkedin.com/in/jakub-sobota-/">Jakub Sobota</a>.
+    </div>
+  )
+}
+
+export default Attribution
